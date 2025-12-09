1bb0a5c7800fc1850fb7bf793aafbb28781c8ef2
omar emad <omaremad11113@gmail.com>
2025-12-09 03:29:16 +0200

    111 ended

---
+++ 1/summerCamp/day-4/B_Shifts_and_Sorting.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cin.tie(nullptr);
+
+void sol()
+{
+
+    return;
+}
+
+int32_t main()
+{
+    FIO;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        sol();
+    }
+    return 0;
+}
