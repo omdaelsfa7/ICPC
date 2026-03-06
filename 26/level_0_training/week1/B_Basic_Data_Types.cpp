06474cb39663888f4f266e8ac52424441ec29fc0
Omdaelsfa7 <omaremad11113@gmail.com>
2026-03-06 05:02:57 +0200

    day1 _yarab_fok_de2yt_oday

---
+++ 26/level_0_training/week1/B_Basic_Data_Types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    int x ;
+    long long y ; 
+    char c ;
+    float f; 
+    double d ;
+    cin >> x >> y >> c >> f >> d ;
+    cout << x << "\n" << y << "\n" << c << "\n" << f << "\n" << d ;
+    return 0; 
+}
